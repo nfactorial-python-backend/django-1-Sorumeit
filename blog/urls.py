b345b73d6ea2f4fd48d09a8a678026f3bcd17a3b
Sorumeit <129141855+Sorumeit@users.noreply.github.com>
2023-08-08 11:19:33 +0000

    123

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ urlpatterns = [
 	path('', views.home, name='blog-home'),
     path('<int:first>/add/<int:second>/' , views.sum , name = 'blog.sum' ),
     path('transform/<str:text>/' , views.up , name = 'blog.upper' ),
-    path('check/<str:word>/' , views.pol , name = 'blog.pol' )
+    path('check/<str:word>/' , views.pal , name = 'blog.pol' )
 ]
