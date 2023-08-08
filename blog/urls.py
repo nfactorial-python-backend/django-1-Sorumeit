c2f7847b186ad2eed2bec94d0e47a5208763a2bd
Sorumeit <129141855+Sorumeit@users.noreply.github.com>
2023-08-08 11:05:52 +0000

    123

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-	path('nfactorial/', views.home, name='blog-home'),
-    path('nfactorial/<int:first>/add/<int:second>/' , views.sum , name = 'blog.sum' )
+	path('', views.home, name='blog-home'),
+    path('<int:first>/add/<int:second>/' , views.sum , name = 'blog.sum' )
 ]
