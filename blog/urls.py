308d24bf9d5a8f41a958d7904f129a7fecfd3743
Sorumeit <129141855+Sorumeit@users.noreply.github.com>
2023-08-08 11:25:32 +0000

    123

--- blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-	path('', views.home, name='blog-home'),
-    path('<int:first>/add/<int:second>/' , views.sum , name = 'blog.sum' ),
-    path('transform/<str:text>/' , views.up , name = 'blog.upper' ),
-    path('check/<str:word>/' , views.pal , name = 'blog.pol' )
-]
