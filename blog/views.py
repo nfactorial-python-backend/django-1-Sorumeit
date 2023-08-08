5118403cd76223cd258b1abc5600bec8c8a03387
Sorumeit <129141855+Sorumeit@users.noreply.github.com>
2023-08-08 11:09:48 +0000

    123

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,5 @@ def home(request):
 	return HttpResponse('<h1>Hello, nfactorial school!</h1>')
  
 def sum(request , first : int , second : int ):
-	return HttpResponse('<h1> {{ first + second }} </h1>')
+	sum1 = first + second
+	return HttpResponse( sum1 )
